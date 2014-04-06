e783f7f62ce212929c4ec0b7866b20fd1c8da41d
kman007us <kevin.m.meredith@gmail.com>
2014-04-06 14:44:32 -0400

    Added Ord i to type signature. as LYAH says, Num is not a sub-type of Ord

--- Replicate.hs
+++ Replicate.hs
@@ -1,5 +1,6 @@
 -- replicate count item_to_replicate
-replicate' :: Int -> a -> [a]
+-- added `Ord` per LYAH
+replicate' :: (Num i, Ord i) => i -> a -> [a]
 replicate' n x 
   | n <= 0    = []
   | otherwise = [x] ++ replicate' (n - 1) x
