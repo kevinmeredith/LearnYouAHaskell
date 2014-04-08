185bf7194d3c9e81438ca34c440a40e18cbe6bb2
kman007us <kevin.m.meredith@gmail.com>
2014-04-07 21:39:11 -0400

    updated to 'I think' is a working version of Quicksort.hs

--- Quicksort.hs
+++ Quicksort.hs
@@ -1,20 +1,24 @@
 split' :: (Ord a) => [a] -> a -> Ordering -> [a]
 split' [] _ _ = []
-split' (x:xs) a LT 
+split' (x:xs) a LT
   | x < a     = x : split' xs a LT
-  | otherwise = split' xs a LT
-split' (x:xs) a GT 
-  | x >= a     = x : split' xs a GT
+  | otherwise = split' xs a LT 
+split' (x:xs) a GT
+  | x > a     = x : split' xs a GT
   | otherwise = split' xs a GT
 
---quicksort' :: (Ord a) => [a] -> [a]
-quicksort' [] = []
-quicksort' xs 
-	| sorted' ys == True = xs
-	| otherwise = quicksort' xs
-	where ys = split' xs (head xs) LT ++ split' xs (head xs) GT
+quicksort :: (Ord a) => [a] => [a]
+quicksort [] = []
+quicksort (x:xs) 
+   | isSorted' rs rs (head rs) = rs
+   | otherwise = quicksort left ++ [x] ++ quicksort right
+     where left = split' xs x LT
+           right = split' xs x GT
+           rs = left ++ [x] ++ right
 
-sorted' :: (Ord a) => [a] -> Bool
-sorted' []     = True
-sorted' [x]    = True
-sorted' (x:xs) = if x > (head xs) then False else sorted' xs
+isSorted' :: (Ord a) => [a] -> [a] -> a -> Bool
+isSorted' [] [] _ = True
+isSorted' (x:xs) [] _ = isSorted' xs xs (head xs)
+isSorted' orig (x:xs) a
+  | a > x = False
+  | otherwise = isSorted' orig xs a
