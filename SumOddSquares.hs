8c3beec2058a237974d673c8a1062e7a80f5bec9
Kevin Meredith <kevin.meredith@mckesson.com>
2014-04-12 19:29:40 -0400

    fixed my work when I saw my answer was wrong from LYAH. I was incorrectly calculating the sum of sumSquares [1..n] where n would begin at 1. So I was summing over [1 (1^2), 1 (1^2), 10 (1^2 + 3^2), ...].

--- SumOddSquares.hs
+++ SumOddSquares.hs
@@ -1,16 +1,14 @@
 sum' :: (Num a) => a -> a -> a
-sum' x y = x + y                        
+sum' x y = x + y                   
 
--- find the sum of all odd squares for [1..n] where
--- n is an argument
-sumOddSquares :: Integer -> Integer
-sumOddSquares n = let odd' x = (x `mod` 2) == 1
-                      odds = filter odd' [1..n]
-                      oddsSq = map (^2) odds
-                      oddsSqSum = foldl sum' 0 oddsSq
-                  in oddsSqSum
+odd' :: (Integral a) => a -> Bool
+odd' x = mod x 2 == 1     
 
 -- find the sum of all odd squares that are smaller than 10,000
 sumAllOddSquaresLTTenThousand :: Integer
-sumAllOddSquaresLTTenThousand = foldl sum' 0 sums
-                        where sums = takeWhile (< 10000) (map sumOddSquares [1..])
+sumAllOddSquaresLTTenThousand = foldl sum' 0 lessThan10k
+                        where odds = filter odd' [1..]
+                              squared = map (^2) odds 
+                              lessThan10k = takeWhile (< 10000) squared
+
+
