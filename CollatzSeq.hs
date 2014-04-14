b73714d732c47aa544df2c3e73918f8dfce4d3b4
Kevin Meredith <kevin.meredith@mckesson.com>
2014-04-13 22:40:39 -0400

    added comment for 'how many Collatz Sequences, between 1..100, are greater than 15

--- CollatzSeq.hs
+++ CollatzSeq.hs
@@ -10,3 +10,15 @@ collatz x
  | otherwise = g x : collatz (g x)
      where f y = y*3 + 1
            g y = y `div` 2   -- per http://stackoverflow.com/a/23050795/409976
+
+-- for all starting numbers between 1 and 100, how many chains 
+-- have a length greater than 15? 
+
+collatzChainLength :: (Integral a) => a -> Int
+collatzChainLength x = length (takeWhile (/= 1) (collatz x) )
+
+--*Main> let xs = map collatzChainLength [1..100]
+--*Main> let ys = map (> 15) xs
+--*Main> let convertBool x = if x then 1 else 0
+--*Main> sum (map convertBool ys)
+--60
