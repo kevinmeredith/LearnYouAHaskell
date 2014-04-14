14aa57a4c65d26836dc963bba8a00d3df1a817aa
Kevin Meredith <kevin.meredith@mckesson.com>
2014-04-13 22:03:27 -0400

    added to LYAH's version of sumOddSquares

--- SumOddSquaresLYAH.hs
+++ SumOddSquaresLYAH.hs
@@ -1,4 +1,8 @@
 -- LYAH. Definitely cleaner. Thinking of Scala Days 2013 key note, I'm thinking 
 -- out loud whether it's better to break out each of the lists...
-sumOddSquatesLt10k :: Integer
-sumOddSquatesLt10k = sum ( takeWhile (< 10000) (map (^2) (filter odd [1..])) )
+sumOddSquaresLt10k :: Integer
+sumOddSquaresLt10k = sum ( takeWhile (< 10000) (map (^2) (filter odd [1..])) )
+
+--using list comprehension
+sumOddSquaresLt10kListComp :: Integer
+sumOddSquaresLt10kListComp = sum (takeWhile (<10000) [n^2 | n <- [1..], odd (n^2)])
