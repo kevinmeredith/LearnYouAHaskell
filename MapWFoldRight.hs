03c33a9d7ec538c409ed78fbdf201ee2fa1f42b8
Kevin Meredith <kevin.meredith@mckesson.com>
2014-04-20 11:12:48 -0400

    removed example of foldl usage in comment

--- MapWFoldRight.hs
+++ MapWFoldRight.hs
@@ -1,8 +1,5 @@
---implement map function withRightFold
 map' :: (a -> b) -> [a] -> [b]
 map' f ys = foldr (\x acc -> (f x) : acc) [] ys -- I appended to `acc`, but LYAH prepends to it, plus signature of lambda
-
---elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
 
 -- [1 2 3]
 -- foldr goes from right to left
