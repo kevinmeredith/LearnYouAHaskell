94b49d75869f663bb1c4a57fa723a75134d4bc2b
Kevin Meredith <kevin.meredith@mckesson.com>
2014-04-24 22:58:42 -0400

    minor change to avoid traversing whole list with fold

--- DistinctNub.hs
+++ DistinctNub.hs
@@ -5,8 +5,14 @@ nub' xs = nubHelper xs []
 
 nubHelper :: (Eq a) => [a] -> [a] -> [a]
 nubHelper []     acc = acc
-nubHelper (y:ys) acc = if(contains y acc)  
+nubHelper (y:ys) acc = if(contains' y acc)  
                          then nubHelper ys acc 
                          else nubHelper ys (acc ++ [y])
-                         where contains match acc = 
-                         	foldl (\as x -> if(y == x) then True else as) False acc
+                         --where contains match acc = 
+                         --	foldl (\as x -> if(y == x) then True else as) False acc
+
+contains' :: (Eq a) => a -> [a] -> Bool
+contains' _ [] = False
+contains' x (y:ys)
+   | x == y = True
+   | otherwise = contains' x ys
