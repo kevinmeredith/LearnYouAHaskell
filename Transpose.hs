f7536126fff7f04ed8db18a77c6fa90f5f1bddbd
Kevin Meredith <kevin.meredith@mckesson.com>
2014-04-28 22:27:04 -0400

    fixed Transpose!

--- Transpose.hs
+++ Transpose.hs
@@ -1,8 +1,9 @@
 --transpose transposes a list of lists. If you look at a list of lists 
 -- as a 2D matrix, the columns become the rows and vice versa.
 transpose' :: [[a]] -> [[a]]
-transpose' [[]] = [[]]
-transpose' ys = map head' ys ++ transpose' (map tail' ys)
+transpose' ys
+   | null $ filter (not . null) ys = []
+   | otherwise = [flatten' $ map head' ys] ++ transpose' (map tail' ys)
 
 head' :: [a] -> [a]
 head' []     = []
@@ -11,6 +12,9 @@ head' (x:xs) = [x]
 tail' :: [a] -> [a]
 tail' []     = []
 tail' (x:xs) = xs
+
+flatten' :: [[a]] -> [a]
+flatten' as = foldl (\acc x -> acc ++ x)  [] as
 
 -- transpose [[1,2,3],[4,5,6],[7,8,9]]  
 -- [[1,4,7],[2,5,8],[3,6,9]]
