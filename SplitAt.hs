cb47c6e9db436f5b109824d9f81807310b7eb39d
kman007us <kevin.m.meredith@gmail.com>
2014-05-03 21:19:14 -0400

    fixed splitAt

--- SplitAt.hs
+++ SplitAt.hs
@@ -1,8 +1,8 @@
-splitAt' :: Int -> [a] -> [[a]]
+splitAt' :: Int -> [a] -> ([a], [a])
 splitAt' n ys 
-  | n < 0     = [[], ys]
-  | otherwise = splitAt'' n ys []
-          where splitAt'' a acc 
-            | a == 0    = [(x:acc), xs]
-            | null xs   = [acc, []]
-            | otherwise = splitAt'' (a-1) xs (x:acc)
+      | n < 0     = ([], ys)
+      | otherwise = splitAt'' n ys []
+      where splitAt'' a (x:xs) acc
+                  | a == 0 = (acc, x:xs)
+                  | null xs = (acc ++ [x], [])
+                  | otherwise = splitAt'' (a-1) xs (acc ++ [x])
