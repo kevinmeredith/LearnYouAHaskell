8ee8f46ef6ae42e99b23b0168a0aeb73a769dbf3
kman007us <kevin.m.meredith@gmail.com>
2014-05-27 21:29:56 -0400

    fixed binary tree per LYAH. an item shouldn't be duplicated in the binary tree. Also, < and > behavior doesn't need separate cases.

--- BinaryTreeADT.hs
+++ BinaryTreeADT.hs
@@ -8,7 +8,6 @@ singletonTree x = Node x EmptyTree EmptyTree
 treeInsert :: (Ord a) => a -> Tree a -> Tree a
 treeInsert x EmptyTree = singletonTree x
 treeInsert x (Node a left right)
-  | x > a && right == EmptyTree = Node a left (singletonTree x)
-  | x < a && left == EmptyTree  = Node a (singletonTree x) right
-  | x > a                       = Node a left (treeInsert x right) 
-  | otherwise                   = Node a (treeInsert x left) right
+  | x == a = Node x left right
+  | x > a  = Node a left (treeInsert x right) 
+  | x < a  = Node a (treeInsert x left) right
