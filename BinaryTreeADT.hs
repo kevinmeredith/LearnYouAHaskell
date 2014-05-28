e001df402e9a7c6ad7ea30f8fcb53f6844efa4af
kman007us <kevin.m.meredith@gmail.com>
2014-05-27 21:41:13 -0400

    added LYAH's treeInsert and my partially implemented treeElem (with LYAH improvements)

--- BinaryTreeADT.hs
+++ BinaryTreeADT.hs
@@ -1,4 +1,6 @@
--- LYAH until `treeInsert after EmptyTree`
+-- LYAH 
+-- I  mistakenly failed to account for `x == a` in treeInsert. Also
+-- I did not need to account for left or right being EmptyTree.
 
 data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
 
@@ -11,3 +13,12 @@ treeInsert x (Node a left right)
   | x == a = Node x left right
   | x > a  = Node a left (treeInsert x right) 
   | x < a  = Node a (treeInsert x left) right
+
+-- I originally searched left and right sides. But good that LYAH
+-- showed me the need to only search left or right.
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+  | x == a = True
+  | x > a  = treeElem x right
+  | x < a  = treeElem x right
