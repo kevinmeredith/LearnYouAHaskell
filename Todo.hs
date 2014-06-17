3c10ba99ab4ab3774f9cf879fd2421da03ca3b34
kman007us <kevin.m.meredith@gmail.com>
2014-06-16 22:35:39 -0400

    updated Todo with view, add, and delete functionality

--- Todo.hs
+++ Todo.hs
@@ -1,5 +1,7 @@
 -- view, add and delete tasks
+-- borrows delete from previous LYAH exercise
 import System.Environment
+import System.Directory
 import System.IO
 import Data.List
 
@@ -9,27 +11,52 @@ main = do
 	args <- getArgs
 	progName <- getProgName
 	let maybeAction = validArgs args
-  	execute maybeAction
+  	executeTodo maybeAction
 
-execute :: (Maybe Action) -> IO a
-execute Just Add    = print "Add not supported"
-execute Just View   = view
-execute Just Delete = print "Delete not supported"
-execute None        = print "invalid user input"
+executeTodo :: (Maybe Action) -> IO ()
+executeTodo (Just Add)    = addTodo
+executeTodo (Just View)   = viewTodo
+executeTodo (Just Delete) = deleteTodo
+executeTodo Nothing       = print "invalid user input"
 
-view :: IO a
-view = do
+viewTodo :: IO ()
+viewTodo = do
 	withFile "todo_file.txt" ReadMode (\handle -> do 
 		contents <- hGetContents handle
 		putStr contents)
+	return ()
+
+addTodo :: IO ()
+addTodo = do
+   todoItem <- getLine
+   appendFile "todo_file.txt" (todoItem ++ "\n")	
+
+deleteTodo :: IO () 
+deleteTodo = do        
+    handle <- openFile "todo_file.txt" ReadMode  
+    (tempName, tempHandle) <- openTempFile "." "temp"  
+    contents <- hGetContents handle  
+    let todoTasks = lines contents     
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks     
+    putStrLn "These are your TO-DO items:"  
+    putStr $ unlines numberedTasks  
+    putStrLn "Which one do you want to delete?"     
+    numberString <- getLine     
+    let number = read numberString     
+        newTodoItems = delete (todoTasks !! number) todoTasks     
+    hPutStr tempHandle $ unlines newTodoItems  
+    hClose handle  
+    hClose tempHandle  
+    removeFile "todo_file.txt"  
+    renameFile tempName "todo_file.txt"  
 
 validArgs :: [String] -> Maybe Action
 validArgs (x:[]) = isValidAction x
-validArgs _      = None
+validArgs _      = Nothing
 
 isValidAction :: String -> Maybe Action
 isValidAction x
   | x == "view"   = Just View
   | x == "add"    = Just Add
   | x == "delete" = Just Delete
-  | otherwise     = None
+  | otherwise     = Nothing
