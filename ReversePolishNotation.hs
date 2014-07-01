ee0f3251a32e255fcc1fb33f58d5df040d006f09
kman007us <kevin.m.meredith@gmail.com>
2014-06-30 22:18:02 -0400

    Updated Reverse Polish Notation. Still needs some work for type safety.

--- ReversePolishNotation.hs
+++ ReversePolishNotation.hs
@@ -1,26 +1,22 @@
 --Reverse Polish Notation
 --solveRPN :: (Num a) => String -> a
-solveRPN :: String -> String
-solveRPN xs = head $ foldl (\acc x -> process acc x) [] $ words xs
+solveRPN :: String -> Double
+solveRPN xs = head $ foldl (\acc x -> foldingFunction acc x) [] $ words xs
 
-process :: [String] -> String -> [String]
-process acc elem 
- | isOp elem = reverse $ calculate (e1, elem, e2) : (drop 2 . reverse $ acc)
- | otherwise  = reverse $ elem : acc 
- where (e1, e2) = first2 acc
-
-first2 :: [a] -> (a, a)
-first2 (x:y:_) = (x,y)
-
-calculate :: (String, String, String) -> String
-calculate (x, op, y) 
- | op == "+" = show $ (read x :: Double) + (read y :: Double)
- | op == "-" = show $ (read x :: Double) - (read y :: Double)
- | op == "*" = show $ (read x :: Double) * (read y :: Double)
- | op == "/" = show $ (read x :: Double) / (read y :: Double)
+foldingFunction :: [Double] -> String -> [Double]
+foldingFunction acc elem 
+ | isOp elem  = calculate (take 2 acc) elem : (drop 2 acc)
+ | otherwise  = (read elem :: Double) : acc 
+ 
+calculate :: [Double] -> String -> Double
+calculate (y:x:_) op
+ | op == "+" =  x + y
+ | op == "-" =  x - y
+ | op == "*" =  x * y
+ | op == "/" =  x / y
 
 isOp :: String -> Bool
-isOp x = elem x ["+", "-", "*", "/"]
+isOp x = x `elem` ["+", "-", "*", "/"]
 
 
 -- "10 4 3 + 2 * -"
