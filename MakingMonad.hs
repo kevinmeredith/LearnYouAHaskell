a056dd9ab3492c1e362c3e81a1b452ad425bd014
kman007us <kevin.m.meredith@gmail.com>
2014-08-30 00:00:08 -0400

    capturing progress(?) on MakingMonad - it might not need the foldr - map might be enough

--- MakingMonad.hs
+++ MakingMonad.hs
@@ -4,14 +4,14 @@ import Data.Ratio
 
 newtype Prob a = Prob { getProb :: [(a,Rational)] } deriving Show  
 
-join :: Prob (Prob a) -> Prob a
-join mm = do
+join' :: Prob (Prob a) -> Prob a
+join' mm = do
    m <- mm
    m 
 
 -- without do notation
-join' :: Prob (Prob a) -> Prob a
-join' mm = mm >>= (\x -> x)
+join'' :: Prob (Prob a) -> Prob a
+join'' mm = mm >>= (\x -> x)
 
 instance Functor Prob where  
     fmap f (Prob xs) = Prob $ map (\(x,p) -> (f x,p)) xs  
@@ -19,8 +19,28 @@ instance Functor Prob where
 -- fmap :: Functor f => (a -> b) -> f a -> f b
 
 instance Monad Prob where
-	return x  = Prob [(x, 1%1)]
-	p >>= f   = join (fmap f p)
+	return x = Prob [(x, 1%1)]
+	p >>= f  = join' (fmap f p)    -- matches type, but not conceptually 
+	                               -- correct for this probability example
 
 -- return :: Monad m => a -> m a
 -- (>>=) :: Monad m  => m a -> (a -> m b) -> m b
+
+foldingFunction :: (a, Rational) -> [(a, Rational)] -> [(a, Rational)]
+foldingFunction x acc = (fst x, (snd x * ))
+
+flatten :: Prob (Prob a) -> Prob a
+flatten mm = mm >>= (\m -> map (rduce) (getProb m))     
+        where rduce = foldr foldingFn [] (snd $ getProb)    --(Prob Char) -> [(Char, Ratio)]
+              foldingFn x acc = (fst x, ((snd x * myRatio) :: Rational)) : acc
+              myRatio = (snd $ ) 
+
+-- motivates how flattening should work
+thisSituation :: Prob (Prob Char)
+thisSituation = Prob
+  [( Prob [('a', 1%2),('b',1%2)], 1%4)
+  ,( Prob [('c', 1%2),('d',1%2)], 3%4)
+  ]
+
+-- desired end result:
+-- Prob [('a', 1%8), ('b', 1%8), (c,3%8), (d,3%8)]
