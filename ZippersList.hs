ade359fd4baf3222c3a9cd76b87b8eec8e84425f
kman007us <kevin.m.meredith@gmail.com>
2014-09-07 10:57:28 -0400

    added goForward and goBackward to ZippersList. Added Zipper for File System. Currently working on how to goDown a file system path.

--- ZippersList.hs
+++ ZippersList.hs
@@ -6,6 +6,15 @@ type BreadCrumb a = List a
 stepOne :: (List a, BreadCrumb a) -> (List a, BreadCrumb a)
 stepOne (Cons x xs, cs) = (xs, Cons x cs)
 
+type Zipper a = ([a], [a])
+
+-- using Haskell's List
+goForward :: Zipper a -> Zipper a
+goForward (x:xs, cs) = (xs, x:cs)
+
+goBackward :: Zipper a -> Zipper a
+goBackward (xs, c:cs) = (c:xs, cs)
+
 --ghci> let list = Cons 5 (Cons 10 Empty)
 --ghci> list
 --Cons 5 (Cons 10 Empty)
